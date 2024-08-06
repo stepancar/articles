5ce654ca18905967e8d3ad051cb4189fe496efe2
Stepan Mikhailiuk <istepancar@gmail.com>
2024-08-06 16:53:29 -0700

    feat(*): add solid color maping on image

--- color-channel-mapping/src/solid-color-remapping/index.mjs
+++ color-channel-mapping/src/solid-color-remapping/index.mjs
@@ -1,4 +1,4 @@
-import { hexToRgb, rgbArrayToHex, PalettesSelector } from "../../utils.mjs";
+import { hexToRgb, rgbArrayToHex, PalettesSelector } from "../utils.mjs";
 
 const image = document.getElementById('image');
 const canvas = document.getElementById('canvas');

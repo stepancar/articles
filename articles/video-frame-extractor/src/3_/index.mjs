37ea6e6c92a1f797b0235d3cf9b8e93a256f80bf
stepancar <stepancar@users.noreply.github.com>
2024-11-06 16:09:48 +0000

    deploy: cb3dc795142f45fb6dc753efed7303eae2b91035

---
+++ articles/video-frame-extractor/src/3_/index.mjs
@@ -0,0 +1,56 @@
+import { BackgroundAsset } from './assets/backgroundAsset.mjs';
+import { ClockAsset } from './assets/clockAsset.mjs';
+import { Scene } from './scene.mjs';
+import { TextAsset } from './assets/textAsset.mjs';
+import { ImageAsset } from './assets/imageAsset.mjs';
+import { VideoAsset } from './assets/videoAsset.mjs';
+
+
+/**
+ * 
+ * @param {CanvasRenderingContext2D} ctx 
+ * @param {HTMLImageElement} image 
+ */
+async function startRendering(ctx) {
+    const scene = new Scene(ctx);
+    
+    scene.assets.push(...[
+        new BackgroundAsset({ src: '../image.jpg' }),
+        new ClockAsset({ position: { x: 535, y: 93 } }),
+        new ImageAsset({ src: '../image.jpg', bounds: { x: 80, y: 15, width: 130, height: 200 } }),
+        new VideoAsset({
+            // src: 'https://nickdesaulniers.github.io/netfix/demo/frag_bunny.mp4',
+            // src: 'https://storage.googleapis.com/lumen5-prod-video/video-seek-test.mp4',
+            src: '../stepan_holy_nodejs_all_keyframes.mp4',
+            position: { x: 290, y: 252 },
+            bounds: { width: 510, height: 300 },
+        }),
+        new TextAsset({ position: { x: 545, y: 220 }, text: 'Hello Holyjs!',  }),
+    ]);
+
+    await scene.waitWhenResourceReady();
+
+    async function startRecording() {
+        const framesCount = 300;
+        for (let i = 1; i < framesCount; i++) {
+            await scene.seek(i * 1000 / 30);
+            scene.draw(ctx);
+        }
+    }
+
+    document.querySelector('#record').addEventListener('click', () => {
+        startRecording();
+    });
+}
+
+async function run() {
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    canvas.width  = 1024;
+    canvas.height = 1024;
+
+    document.body.appendChild(canvas);
+    startRendering(ctx);
+}
+
+run();
--- articles/video-frame-extractor/src/resources.mjs
+++ articles/video-frame-extractor/src/resources.mjs
@@ -4,3 +4,12 @@ export async function createImageResource(src) {
     await image.decode();
     return image;
 }
+
+export async function createSourceBufferForVide(src) {
+    const response = await fetch(src);
+    const buffer = await response.arrayBuffer();
+    // prepare for setting to html video element
+    // create blob
+    const blob = new Blob([buffer], { type: 'video/mp4' });
+    return blob;
+}

afc7734b218694365a248a3a722089afa42de07c
Stepan Mikhailiuk <istepancar@gmail.com>
2024-11-07 02:54:16 +0800

    feat(*): add fast seeking

---
+++ articles/video-frame-extractor/src/4_/assets/imageAsset.mjs
@@ -0,0 +1,31 @@
+import { BaseAsset } from "./baseAsset.mjs";
+
+export class ImageAsset extends BaseAsset {
+    constructor({ src, ...settings }) {
+        super(settings);
+        this.image = document.createElement("img");
+        this.image.src = src;
+    }
+
+    /**
+     * 
+     * @param {CanvasRenderingContext2D} ctx 
+     */
+    draw(ctx) {
+        const bounds = this.bounds;
+        const maxWidth = bounds.width;
+        const maxHeight = bounds.height;
+        // draw image with aspect ratio and inside the bounds
+        const aspectRatio = this.image.width / this.image.height;
+        const imageWidth = Math.min(maxWidth, aspectRatio * maxHeight);
+        const imageHeight = Math.min(maxHeight, maxWidth / aspectRatio);
+        const x = bounds.x + (maxWidth - imageWidth) / 2;
+        const y = bounds.y + (maxHeight - imageHeight) / 2;
+        ctx.drawImage(this.image, x, y, imageWidth, imageHeight);
+        
+    }
+
+    async waitWhenResourceReady() {
+        return this.image.decode();
+    }
+}

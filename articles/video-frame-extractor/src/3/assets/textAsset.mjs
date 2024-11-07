4b7482147d8912ef6c7111ee26ad7691aae11732
stepancar <stepancar@users.noreply.github.com>
2024-11-07 05:23:44 +0000

    deploy: 1983a13f9a2e6b36abf9a9a6087dd9b510e7ca1d

--- articles/video-frame-extractor/src/3/assets/textAsset.mjs
+++ articles/video-frame-extractor/src/3/assets/textAsset.mjs
@@ -9,7 +9,7 @@ export class TextAsset extends BaseAsset {
     draw(ctx) {
         const animationDuration = 3000;
         const fontSize = 80 + 5 * (this.currentTime % animationDuration) / animationDuration;
-        ctx.fillStyle = "black";
+        ctx.fillStyle = "white";
         ctx.font = `${fontSize}px Arial`;
         const textMetrix = ctx.measureText(this.text);
         const x = this.position.x - textMetrix.width / 2;

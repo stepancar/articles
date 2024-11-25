a39c229f4c71dd237d57cb86f47e8d83953fc2b4
stepancar <stepancar@users.noreply.github.com>
2024-11-25 06:34:27 +0000

    deploy: 47815c5f4a895ca00c10f869912f6cad0f3704c3

---
+++ articles/device-motion/src/index.mjs
@@ -0,0 +1,38 @@
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+        .register("service-worker.mjs")
+        .then(() => console.log("Service Worker Registered"))
+        .catch((err) =>
+            console.error("Service Worker Registration Failed:", err)
+        );
+}
+
+function updateData(e) {
+    const element = document.getElementById('output');
+    element.innerHTML = JSON.stringify({
+        x: e.accelerationIncludingGravity.x,
+        y: e.accelerationIncludingGravity.y,
+        z: e.accelerationIncludingGravity.z
+    });
+}
+
+async function subscribe() {
+    if (DeviceMotionEvent && DeviceMotionEvent.requestPermission) {
+        const permission = await DeviceMotionEvent.requestPermission();
+        if (permission === "granted") {
+            console.log("Device Motion Permission Granted");
+        }
+        window.addEventListener("devicemotion", updateData);
+    }
+}
+
+document.getElementById("subscribe").addEventListener("click", async () => {
+    subscribe();
+});
+
+try {
+    subscribe();
+} catch (e) {
+    console.log(e);
+}
+

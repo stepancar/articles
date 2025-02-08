a25f80293b9e1b0a2ae24bc32998cfb6b93775f3
Максим <yakhont04@mail.ru>
2025-02-08 21:36:42 +0300

    stile for table | another logic for counting time

--- articles/canvas-draw-image-scale-performance/src/scale/index.mjs
+++ articles/canvas-draw-image-scale-performance/src/scale/index.mjs
@@ -1,39 +1,40 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-const image1 = new Image();
-const image2 = new Image();
-image1.src = 'Lenna_512x512.png';
-image2.src = 'Lenna_256x256.png';
+const image = new Image();
+image.src = 'Lenna_512x512.png';
 
 class Test_Image {
-    constructor(name, image512, image256) {
+    constructor(name, image) {
         this.name = name;
-        this.image512 = image512;
-        this.image256 = image256;
+        this.image = image;
     }
 }
 
-const lenna = new Test_Image('Lenna', image1, image2);
+const lenna = new Test_Image('Lenna', image);
 document.querySelector('#image-name').innerHTML = lenna.name;
 
-image1.onload = () => {
+function scale_image() {
     const start = performance.now();
     for (let i = 0; i < 1000; i++) {
-        ctx.drawImage(lenna.image512, 0, 0, 512, 512, 0, 0, 1000, 1000);
+        ctx.drawImage(lenna.image, 0, 0, 512, 512, 0, 0, 1000, 1000);
+        ctx.scale(1, 10);
     }
     const end = performance.now();
 
-    document.querySelector('#size-512').innerHTML = end - start;
-    console.log(`Drawing 1000 images took ${end - start}ms`);
-};
+    document.querySelector('#scale').innerHTML = end - start;
+    console.log(`Scale 1000 images took ${end - start}ms`);
+}
 
-image2.onload = () => {
+image.onload = () => {
     const start = performance.now();
     for (let i = 0; i < 1000; i++) {
-        ctx.drawImage(lenna.image256, 0, 0, 256, 256, 0, 0, 1000, 1000);
+        ctx.drawImage(lenna.image, 0, 0, 512, 512, 0, 0, 1000, 1000);
+        ctx.scale(1, 10);
+
     }
     const end = performance.now();
 
-    document.querySelector('#size-256').innerHTML = end - start;
+    document.querySelector('#full-size').innerHTML = end - start;
     console.log(`Drawing 1000 images took ${end - start}ms`);
+    scale_image();
 };

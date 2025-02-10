ff4a2b2b9807af23fc903dfad8397ee2fc832c3d
Максим <yakhont04@mail.ru>
2025-02-10 11:23:05 +0300

    small fix

--- articles/canvas-draw-image-scale-performance/src/scale/index.mjs
+++ articles/canvas-draw-image-scale-performance/src/scale/index.mjs
@@ -1,7 +1,9 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+const canvas_1 = document.getElementById('canvas-1');
+const canvas_2 = document.getElementById('canvas-2');
+const ctx_1 = canvas_1.getContext('2d');
+const ctx_2 = canvas_2.getContext('2d');
 const image = new Image();
-image.src = 'Lenna_512x512.png';
+image.src = 'SIPI_Jelly_Beans_256x256.jpg';
 
 class Test_Image {
     constructor(name, image) {
@@ -15,24 +17,29 @@ document.querySelector('#image-name').innerHTML = lenna.name;
 
 function scale_image() {
     const start = performance.now();
-    for (let i = 0; i < 1000; i++) {
-        ctx.drawImage(lenna.image, 0, 0, 512, 512, 0, 0, 1000, 1000);
-        ctx.scale(1, 10.1);
+    for (let i = 0; i < 10000; i++) {
+        ctx_2.drawImage(lenna.image, 0, 0, 256, 256, 0, 0, 256, 256);
+        ctx_2.scale(2, 2);
     }
     const end = performance.now();
 
     document.querySelector('#scale').innerHTML = end - start;
-    console.log(`Scale 1000 images took ${end - start}ms`);
+    console.log(`Drawing and scaling 1000 images took ${end - start}ms`);
 }
 
-image.onload = () => {
+function draw_image() {
     const start = performance.now();
-    for (let i = 0; i < 1000; i++) {
-        ctx.drawImage(lenna.image, 0, 0, 512, 512, 0, 0, 1000, 1000);
+    for (let i = 0; i < 10000; i++) {
+        ctx_1.drawImage(lenna.image, 0, 0, 256, 256, 0, 0, 256, 256);
+        // ctx_1.scale(2, 2);
     }
     const end = performance.now();
 
     document.querySelector('#full-size').innerHTML = end - start;
     console.log(`Drawing 1000 images took ${end - start}ms`);
     scale_image();
+}
+
+image.onload = () => {
+    draw_image();
 };

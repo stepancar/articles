6540519d943aa5c3427e9c0ecea5b3ee67866ccd
dustov2003 <dustov2003@gmail.com>
2025-02-15 17:37:27 +0300

    second week progress

---
+++ articles/video-transcoding-in-web/test_videos/transcode_script.py
@@ -0,0 +1,69 @@
+import os
+import subprocess
+import time
+
+
+def process_videos():
+    output_dir = "transcoded_video"
+
+    metrics_filename = "metrics.csv"
+    metrics_arr = [["from","to","resolution","time"]]
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    # Расширения видеофайлов для поиска
+    video_extensions = ("mp4","mkv", "avi", "mov", "flv", "wmv")
+    to_formats = (
+    "avi",
+    "mpg",
+    "flv",
+    "webm",
+    "mkv")
+    for format in to_formats:
+        if not os.path.exists(f"{output_dir}/{format}"):
+                os.makedirs(f"{output_dir}/{format}")
+
+    # Находим все видеофайлы в текущей директории
+    video_files = [f for f in os.listdir('.') if os.path.isfile(f) and f.lower().endswith(video_extensions)]
+
+    # Обрабатываем каждый найденный файл
+    for video_file in video_files:
+
+        for format in to_formats:
+            print(f"Processing {video_file}... to {format}")
+            base_name, src_format = os.path.splitext(video_file)
+            resolution = "*".join(base_name.split("_")[1:])
+            output_file = os.path.join(output_dir, f"{format}/{base_name}.{format}")
+
+            try:
+                start_time = time.time()
+                result = subprocess.run(
+                    [
+                        "ffmpeg",
+                        "-i",
+                        video_file,
+                        output_file
+                    ],
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                    text=True,
+                    check=True
+                )
+                end_time = time.time()
+                elapsed_time = end_time - start_time
+                metrics_arr.append([src_format[1:],format,resolution,f"{elapsed_time:.2f}"])
+                print(f"Transcoding of {video_file} completed in {elapsed_time:.2f} seconds.")
+
+            except subprocess.CalledProcessError as e:
+                print(f"Error processing {video_file}: {e.stderr}")
+
+    print(f"All videos transcoded.Transcoded video were saved saved in {output_dir}/")
+    with open(metrics_filename, "w", encoding="utf-8") as metrics_file:
+        for m in metrics_arr:
+            metrics_file.write(",".join(m)+"\n")
+
+
+
+if __name__ == "__main__":
+    process_videos()

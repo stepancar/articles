5c5f9e7107527ccf615a5c656474b029e2626dc9
Максим <yakhont04@mail.ru>
2025-02-22 17:42:29 +0300

    user can enter size of canvas

--- articles/canvas-draw-image-scale-performance/src/scale/index.mjs
+++ articles/canvas-draw-image-scale-performance/src/scale/index.mjs
@@ -7,11 +7,21 @@ canvas_2.height = 1080;
 const ctx_1 = canvas_1.getContext('2d');
 const ctx_2 = canvas_2.getContext('2d');
 const image = new Image();
-image.src = 'Lenna_512x512.png';
+image.src = 'https://upload.wikimedia.org/wikipedia/ru/thumb/2/24/Lenna.png/330px-Lenna.png';
 const addImageButton = document.querySelector('#imageProcess');
-const clearCanvasButton = document.querySelector('#clearCanvas');
 const ResultTable = document.querySelector('.ResultTable');
 let iterationsCount = 10000;
+
+const setSizeOfTheCanvas = () => {
+    let width = document.querySelector('#canvasWidth').value;
+    let height = document.querySelector('#canvasHeight').value;
+    console.log(width);
+    console.log(height);
+    canvas_1.width = width;
+    canvas_1.height = height;
+    canvas_2.width = width;
+    canvas_2.height = height;
+}
 
 const setIterationsCount = () => { 
     iterationsCount = document.querySelector('#iterations_cnt').value;
@@ -19,16 +29,16 @@ const setIterationsCount = () => {
 
 const loadImage = () => {
     image.src = document.querySelector('#pic_url').value;
+}
+
+const takeMeasurements = () => {
+    console.log("123");
+    loadImage();
     setIterationsCount();
+    setSizeOfTheCanvas();
 }
 
-const clearCanas = () => {
-    ctx_1.reset();
-    ctx_2.reset();
-}
-
-addImageButton.addEventListener('click', loadImage);
-clearCanvasButton.addEventListener('click', clearCanas);
+addImageButton.addEventListener('click', takeMeasurements);
 
 class Test_Image {
     constructor(name, image) {
@@ -42,7 +52,9 @@ const lenna = new Test_Image('Lenna', image);
 function draw_image_without_scale() {
     const start = performance.now();
     for (let i = 0; i < iterationsCount; i++) {
-        ctx_2.drawImage(lenna.image, 0, 0);
+        let x = Math.random() * canvas_1.width;
+        let y = Math.random() * canvas_1.height;
+        ctx_2.drawImage(lenna.image, x, y);
     }
     const end = performance.now();
     let time = end - start;
@@ -53,7 +65,9 @@ function draw_image_without_scale() {
 function draw_image_with_scale() {
     const start = performance.now();
     for (let i = 0; i < iterationsCount; i++) {
-        ctx_1.drawImage(lenna.image, 0, 0);
+        let x = Math.random() * 100;
+        let y = Math.random() * 100;
+        ctx_1.drawImage(lenna.image, x, y);
         ctx_1.scale(2, 2);
     }
     const end = performance.now();
@@ -76,14 +90,11 @@ function addNewMeasurements(drawImageWithScaleTime, drawImageWithoutScaleTime) {
     let cell1 = newRow.insertCell();
     let cell2 = newRow.insertCell();
     let cell3 = newRow.insertCell();
+    let cell4 = newRow.insertCell();
     
     cell0.innerHTML = cnt++;
     cell1.innerHTML = drawImageWithScaleTime;
     cell2.innerHTML = drawImageWithoutScaleTime;
     cell3.innerHTML = iterationsCount;
+    cell4.innerHTML = canvas_1.width + "x" + canvas_1.height;
 }
-
-// 1) ссылку на картинку (по умолчаню lena, к примеру) и есть список других подготовленных картинок 
-// 2) размеры канваса
-// 3) количество итераций
-// и еще момент. Попробуйте рисовать картинку по рандомным координатам

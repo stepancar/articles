7f1c654b9efee8e231370c391881a8a88deee267
Stepan Mikhailiuk <istepancar@gmail.com>
2025-03-20 14:33:56 +0800

    feat(*): add location assign demo

--- articles/pwa-open-pdf/src/service-worker.mjs
+++ articles/pwa-open-pdf/src/service-worker.mjs
@@ -10,5 +10,13 @@ self.addEventListener("install", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
-    
+    const url = new URL(event.request.url);
+
+    // Check if the request is for a PDF file under `files.stepancar.github.io`
+    if (url.hostname === "files.stepancar.github.io" && url.pathname.endsWith(".pdf")) {
+        const newUrl = `https://stepancar.github.io${url.pathname}`;
+        console.log(`Redirecting: ${event.request.url} -> ${newUrl}`);
+
+        event.respondWith(fetch(newUrl, { headers: event.request.headers }));
+    }
 });
